cb36b3f27f00d676e93944b76e08ac82fc05c914
Minilay8 <minilie88@gmail.com>
2019-06-20 19:10:35 +0600

    adapted on smartphones

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     console.log("Aloh");   
     $("#home").click(function()
     {
-        document.querySelector("#layout1").scrollIntoView({
+        document.querySelector("body").scrollIntoView({
             behavior: 'smooth'});
     });
     $("#navedu").click(function()
@@ -36,4 +36,25 @@ $(document).ready(function()
         document.querySelector("#contact").scrollIntoView({
             behavior: 'smooth'});
     });
+    var cnt0 = 0;
+    if($("body").width() <= 600)
+    {
+        console.log($("body").width())
+        $("#navbar").click(function()
+        {
+            console.log("nav");
+            if(cnt0 % 2 == 0)
+            {
+                $(this).css("width", "100%");
+                $("#navbar div").css("display", "block");
+            }
+            else
+            {
+                $(this).css("width", "15%");
+                $("#navbar div").css("display", "none");
+            }
+            cnt0++;
+        });
+    }
+    
 });
