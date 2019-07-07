1c0ec515f4e1fd8a3016f3b8f50b462c669c0e93
Minilay8 <minilie88@gmail.com>
2019-07-07 22:43:38 +0600

    photo #back1 fixed

--- main.js
+++ main.js
@@ -11,11 +11,6 @@ $(document).ready(function()
         document.querySelector("#education").scrollIntoView({
             behavior: 'smooth'});
     }); 
-    $("#navcamp").click(function()
-    {
-        document.querySelector("#camp").scrollIntoView({
-            behavior: 'smooth'});
-    });
     $("#navawards").click(function()
     {
         document.querySelector("#awards").scrollIntoView({
@@ -63,7 +58,7 @@ $(document).ready(function()
     $(window).scroll(function () {
         currentY = $('#background').offset().top;
         breakpoint = $("#education").position().top;
-        var breakpoint1 = $("#camp").position().top
+        var breakpoint1 = $("#awards").position().top
         var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
         // console.log(currentY);
         $("#background").css("transform","translateY("+(-currentY / 3)+"px)")
