88747aa5c98431fad209db34eb0ca747fad998e0
Minilay8 <minilie88@gmail.com>
2019-08-10 02:37:02 +0600

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,12 @@
 console.log('Hola');
 $(document).ready(function()
 {
-
-    $("#courses_btn").click(function(){
-        document.querySelector("#courses").scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-    $("#about_btn").click(function(){
-        document.querySelector("#about").scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-    $("#contact_btn").click(function(){
-        document.querySelector("footer").scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-    $("#background_abt").click(function(){
-        document.querySelector("#about").scrollIntoView({
+    $('#nav-menu>ul>li>a').click(function()
+    {
+        var btn_id = $(this).attr('id');
+        btn_id = btn_id.substr(0, btn_id.length - 4);
+        console.log(btn_id);
+        document.querySelector("#"+btn_id).scrollIntoView({
             behavior: 'smooth'
         });
     });
@@ -29,23 +17,37 @@ $(document).ready(function()
     {
         $(this).find(".card_img").css("filter","contrast(0.5)");
     });
-    console.log(window.location.pathname);
-    for(var i = 1; i <= 6; i++)
+
+    $(".cards").click(function(){
+        $('#layout').css("display","block");
+        $('#enroll_window').css("display","block");
+        $("html").css("overflow","hidden");
+    });
+    $('#layout').click(function()
     {
-        // $("#card_img_"+i).css("background-image","url('./assets/img/card_img"+i+".png');");
-        $("#card_img_"+i).css("background-image","url('./assets/img/card_img"+i+".png')");
-        console.log("assets/img/card_img"+i+".png");
-    }
-    var width = window.innerWidth;
-    if(width < 768)
+        $(this).css("display","none");
+        $('#enroll_window').css("display","none");
+        $("html").css("overflow","auto");
+    });
+
+    var width = document.documentElement.clientWidth;
+    if(width < 990)
     {
         $("#nav-menu ul li").click(function()
         {
+            console.log('Hola');
             $('#nav-menu').removeClass("in");
 
             $('#nav-menu').css('height','1px');
         });
 
         $("footer *").addClass("text-center");
+    }
+
+    for(var i = 1; i <= 6; i++)
+    {
+        // $("#card_img_"+i).css("background-image","url('./assets/img/card_img"+i+".png');");
+        $("#card_img_"+i).css("background-image","url('./assets/img/card_img"+i+".png')");
+        console.log("assets/img/card_img"+i+".png");
     }
 });
