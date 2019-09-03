854a338e70f791bfaa3d42fb4e38e7b5320d15e2
Minilay8 <minilie88@gmail.com>
2019-09-03 19:24:48 +0600

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,11 @@ $(document).ready(function()
             behavior: 'smooth'
         });
     });
+    $('#courses_abt').click(function(){
+        document.querySelector("#courses").scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
     $(".cards").hover(function()
     {
         $(this).find(".card_img").css("filter","contrast(1)");
@@ -23,25 +28,41 @@ $(document).ready(function()
         $(this).find(".card_img").css("filter","contrast(0.5)");
     });
 
-    $(".card_img, .card_cont").click(function(){
+    function layout_hide()
+    {
+        $('#layout').css("display","none");
+        $('#course_window').css("display","none");
+        $("html").css("overflow","auto");
+        $('.cont').css("display","none");
+    }
+    $(".cards a").click(function(){
         $('#layout').css("display","block");
         $('#course_window').css("display","block");
         $("html").css("overflow","hidden");
         var course_title = $(this).find('h3').text();
-        $("#course_window").find('h4').text(course_title);
+        $("#course_window").find('.card_cont').find("h3").text(course_title);
+        var course_link = $(this).data('link');
+        $('#course_window .btn-primary').attr('href',course_link);
+        $('#course_window .btn-danger').click(function()
+        {
+            layout_hide();
+        });
+        var course_id = $(this).data("id");
+        console.log(course_id);
+        $("#cont_"+course_id).css('display','block');
+
     });
     $('#layout').click(function()
     {
-        $(this).css("display","none");
-        $('#course_window').css("display","none");
-        $("html").css("overflow","auto");
+        layout_hide();
     });
 
     if(window.innerWidth < 994)
     {
-        $('.navbar-collapse a').click(function (e) {
+        $('.navbar-collapse a').click(function (e) 
+        {
             $('.navbar-collapse').collapse('toggle');
-          });
+        });
     }
       
     for(var i = 1; i <= 6; i++)
